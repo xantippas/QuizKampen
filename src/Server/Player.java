be3fb80df55246cb2f7eb17b4aa386c09b924c0d
xantippas <xantippa.hogberg@gmail.com>
2021-11-16 19:38:59 +0100

    created player branch and added player class to server package

---
+++ src/Server/Player.java
@@ -0,0 +1,16 @@
+package Server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    int questionCounter;
+    int playerPoints;
+    String currentCategory;
+    boolean hasAnswered;
+    List<Player> playesInGame = new ArrayList<>();
+
+    
+
+
+}
