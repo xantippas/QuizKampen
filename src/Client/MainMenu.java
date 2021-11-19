37848ecb8e63b5cd1059cf7fbd853fca6ae9ec33
xantippas <xantippa.hogberg@gmail.com>
2021-11-19 08:47:58 +0100

    added styling to mainmenu class, colors to be corrected later, layout complete

--- src/Client/MainMenu.java
+++ src/Client/MainMenu.java
@@ -2,6 +2,7 @@ package Client;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import javax.swing.border.SoftBevelBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -19,13 +20,16 @@ public class MainMenu extends JFrame implements ActionListener {
         mainPanel.setLayout(null);
 
         title.setPreferredSize(new Dimension(250,200));
-        title.setBounds(80,50,250,200);
-        title.setFont(new Font("Montserrat", Font.BOLD, 12));
+        title.setBounds(4,50,400,200);
+        title.setFont(new Font("Montserrat", Font.BOLD, 18));
+        //title.setForeground(Color.white); //color of text
         play.setBounds(80,280,250,50);
+        play.setFont(new Font("Montserrat", Font.BOLD, 18));
+        play.setForeground(Color.white);
+        play.setBackground(Color.BLUE);
 
         play.addActionListener(this);
         optionPanel.add(play);
-
 
         mainPanel.setBackground(Color.CYAN);
         mainPanel.add(title);
