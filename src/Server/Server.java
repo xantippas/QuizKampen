8fa2dc3ab19897106a96da3dcd95be04ed25373c
T.Yokoi <laylalayla2905@gmail.com>
2021-11-20 13:09:05 +0100

    developed socket in server side

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -12,32 +13,45 @@ import java.util.List;
 public class Server {
     public static void main(String[]args) throws IOException {
         List<Player> playerList = new ArrayList<>();
+        Game game = new Game();
+        int portNummer = 8765;
+        ServerSocket serverSocket = new ServerSocket();
+        serverSocket.bind(new InetSocketAddress("127.0.0.1", portNummer));
 
-        int portNummer = 12345;
-        ServerSocket serverSocket = new ServerSocket(portNummer);
+        try (
+                Socket socket = serverSocket.accept();
+                // from server to client
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+                // from client to server
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                ) {
+            System.out.println("socket is open in server");
+                    String fromClient;
+                    while (true) {
+                        fromClient = bufferedReader.readLine();
+                        try{
+                            int num = Integer.parseInt(fromClient);
 
-        while (true) {
-
-            try (
-                    Socket socket = serverSocket.accept();
-                    PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
-
-                String toClient;
-                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-                Game game = new Game();
-
-                while ((toClient = in.readLine()) != null) {
-                    printWriter.println(toClient);
-                    //Game game = new Game(Player)
-                }
-
-
-            } catch (Exception e) {
-                e.printStackTrace();
+                            switch (num) {
+                                case 1:
+                                    printWriter.println("server get num 1 from client");
+                                    break;
+                                case 2:
+                                    printWriter.println("server get num 2 from client");
+                                    break;
+                                default:
+                                    printWriter.println("server get unregistered number from client");
+                            }
+                        }catch(NumberFormatException e){
+                            printWriter.println("should be a number");
+                        }
+                        if(fromClient.equals("bye")) {
+                            break;
+                        }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         }
     }
-}
