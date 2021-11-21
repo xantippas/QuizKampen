382500768e306d74cac201bf44a445ece6b98399
T.Yokoi <laylalayla2905@gmail.com>
2021-11-21 16:03:00 +0100

    added socket in the root component(Client Class) instead of having the socket in a separate class

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -12,12 +12,30 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client extends JFrame {
-    ClientSocket clientSocket = new ClientSocket();;
-    MainMenu menu = new MainMenu(clientSocket);
+
+    // experiment purpose
+    Socket socket;
+    PrintWriter printWriter;
+    BufferedReader bufferedReader;
+    int portNummer = 8765;
+    String hostName = "127.0.0.1";
+
+    //ClientSocket clientSocket = new ClientSocket();;
+    MainMenu menu;
     QuizPanel panel = new QuizPanel();
 
-
     Client() {
+        try {
+            this.socket = new Socket(hostName, portNummer);
+            System.out.println("socket is open in client");
+            // from client to server
+            this.printWriter = new PrintWriter(socket.getOutputStream(), true);
+            // from server to client
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        menu = new MainMenu(socket, printWriter, bufferedReader);
         setTitle("Quiz Kampen");
         add(menu.createMenu());
         //add(panel.quizWindow());
@@ -26,9 +44,34 @@ public class Client extends JFrame {
         setSize(420,420);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        clientSocket.startConnection();
+        // clientSocket.startConnection();
+        startConnection();
+
     }
 
+    public void startConnection() {
+        try {
+            String line;
+            while(true) {
+                line = bufferedReader.readLine();
+                System.out.println("the response from Server " + line);
+                char code = line.charAt(0);
+                System.out.println("code from server is " + code);
+                switch (code) {
+                    case '1':
+                        System.out.println(line);
+                        break;
+                    case '2':
+                        menu.getGameStatusPage().getChooseCategory().getCategoryHistory().setText(line);
+                        break;
+                    default:
+                        System.out.println("To be implemented");
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
 
