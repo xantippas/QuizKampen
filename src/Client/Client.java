d32a17642eb17e29c0ae9dfa3e2a68523499cf62
xantippas <xantippa.hogberg@gmail.com>
2021-11-21 15:04:08 +0100

    two clients can connect to server, srever responds in run method to both clients

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -5,6 +5,7 @@ import Server.Player;
 import javax.swing.*;
 import java.awt.*;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -12,22 +13,25 @@ import java.net.Socket;
 public class Client extends JFrame {
 
     MainMenu menu = new MainMenu();
+    String fromServer;
+    BufferedReader bufferedReader;
+    PrintWriter printWriter;
 
-    public Client(){
+    public Client(String hostName){
         int portNummer = 8765;
-        String hostName = "192.168.0.101";
+        //String hostName = "192.168.0.101";
             try {
                 Socket socket = new Socket(hostName, portNummer);
                 System.out.println("socket is open in client");
                 // from client to server
-                PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+                printWriter = new PrintWriter(socket.getOutputStream(), true);
                 // from server to client
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
                 String toServer;
-                String fromServer;
+
                 // input in client
-                BufferedReader writeInMyText = new BufferedReader(new InputStreamReader(System.in));
+                //BufferedReader writeInMyText = new BufferedReader(new InputStreamReader(System.in));
 
                 //Client c = new Client();
 
@@ -45,11 +49,6 @@ public class Client extends JFrame {
                 if(toServer.equals("bye")){
                     break;
                 }*/
-                while (true){
-                    if ((fromServer = bufferedReader.readLine()) != null) {
-                        System.out.println("the response from Server " + fromServer);
-                    }
-                }
 
 
 
@@ -59,25 +58,22 @@ public class Client extends JFrame {
                 e.printStackTrace();
             }
 
-        /*setTitle("Quiz Kampen");
-        add(menu.createMenu());
-        //add(panel.quizWindow());
+    }
 
-        setResizable(false);
-        setVisible(true);
-        setSize(420,420);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);*/
+    public void play() throws IOException {
+        add(menu.createMenu());
+        while (true){
+            if ((fromServer = bufferedReader.readLine()) != null) {
+                System.out.println("the response from Server " + fromServer);
+            }
+        }
 
     }
 
-    public void play(){
-        add(menu.createMenu());
-    }
 
-
-    public static void main(String[] args) {
-            Client c = new Client();
+    public static void main(String[] args) throws IOException {
+            String hostName = "192.168.0.101";
+            Client c = new Client(hostName);
             c.setResizable(false);
             c.setVisible(true);
             c.setSize(420,420);
