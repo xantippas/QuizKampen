ffc721de80633ca4acd6b01115f419a68b1edf33
xantippas <xantippa.hogberg@gmail.com>
2021-11-24 16:17:57 +0100

    set styling for roundsscore panel

--- src/RoundsScorePanel.java
+++ src/RoundsScorePanel.java
@@ -8,9 +8,9 @@ public class RoundsScorePanel extends JPanel {
         JPanel mainPanel = new JPanel();
         JPanel layoutPanel = new JPanel();
 
-        JLabel titleOfWindow = new JLabel("Poäng:");
-        JLabel titleOne = new JLabel("Player 1");
-        JLabel titleTwo = new JLabel("Player 2");
+        JLabel titleOfWindow = new JLabel("Poäng:", SwingConstants.CENTER);
+        JLabel titleOne = new JLabel("Player 1", SwingConstants.CENTER);
+        JLabel titleTwo = new JLabel("Player 2", SwingConstants.CENTER);
         JLabel playerOneScore = new JLabel();
         JLabel playerTwoScore = new JLabel();
 
@@ -19,26 +19,27 @@ public class RoundsScorePanel extends JPanel {
 
         public RoundsScorePanel(List<Integer> scores){
             mainPanel.setLayout(new BorderLayout());
-            System.out.println(scores);
 
             titleOfWindow.setFont(new Font("Montserrat", Font.BOLD, 18));
 
-            layoutPanel.setLayout(new GridLayout(2,2,5,5));
-            layoutPanel.setPreferredSize(new Dimension(200,200));
+            layoutPanel.setLayout(new GridLayout(2,2));
+            layoutPanel.setPreferredSize(new Dimension(250,180));
             layoutPanel.setBorder(new LineBorder(Color.BLACK));
             layoutPanel.setBackground(Color.white);
-
-            //titleOne.setFont(new Font("Montserrat", Font.PLAIN, 12));
-            //titleTwo.setFont(new Font("Montserrat", Font.PLAIN, 12));
 
             scorePlayerOne = scores.get(0).toString();
             scorePlayerTwo = scores.get(1).toString();
             playerOneScore.setText(scorePlayerOne);
             playerTwoScore.setText(scorePlayerTwo);
+            
+            playerOneScore.setVerticalAlignment(SwingConstants.CENTER);
+            playerTwoScore.setVerticalAlignment(SwingConstants.CENTER);
+            playerOneScore.setHorizontalAlignment(SwingConstants.CENTER);
+            playerTwoScore.setHorizontalAlignment(SwingConstants.CENTER);
 
             layoutPanel.add(titleOne);
-            layoutPanel.add(playerOneScore);
             layoutPanel.add(titleTwo);
+            layoutPanel.add(playerOneScore);
             layoutPanel.add(playerTwoScore);
 
             mainPanel.add(titleOfWindow, BorderLayout.NORTH);
